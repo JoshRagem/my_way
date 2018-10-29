e3d34906471b99c0a1b3165e655378953ad91dfe
jbarney <jbarney@xlfleet.com>
2018-10-29 13:39:34 -0400

    get elvis config from project

--- lib/my_way.ex
+++ lib/my_way.ex
@@ -1,18 +1,10 @@
-defmodule MyWay do
-  @moduledoc """
-  Documentation for MyWay.
-  """
+defmodule Mix.Tasks.Elvis do
+  use Mix.Task
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MyWay.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def run(args) do
+    case :elvis_core.rock(Mix.Project.config[:elvis]) do
+      {:fail, _} -> Mix.raise "mix elvis failed"
+      :ok -> :ok
+    end
   end
 end
--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule MyWay.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      elvis: elvis(),
     ]
   end
 
@@ -21,8 +22,15 @@ defmodule MyWay.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:elvis, "~> 0.4.1", hex: :elvis_core},
     ]
+  end
+
+  defp elvis do
+    [%{
+      dirs: ['src'],
+      filter: '*.erl',
+      ruleset: :erl_files
+    }]
   end
 end
